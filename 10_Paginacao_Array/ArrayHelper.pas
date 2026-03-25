b710211650d371be6727763e6b81354792a93189
Deyvid Augusto <141926332+DeyvidAugusto@users.noreply.github.com>
2026-03-25 17:27:15 -0300

    Add files via upload

---
+++ 10_Paginacao_Array/ArrayHelper.pas
@@ -0,0 +1,25 @@
+unit ArrayHelper;
+
+interface
+type
+   TMyIntArray = array of integer;
+   function IndexOfInteger(const List: TMyIntArray; const Value: Integer): Integer;
+implementation
+
+function IndexOfInteger(const List: TMyIntArray; const Value: Integer): Integer;
+var
+  I: Integer;
+begin
+  Result := -1;
+  for I := Low(List) to High(List) do
+  begin
+    if List[I] = Value then
+    begin
+      Result := I;
+      Break;
+    end;
+  end;
+end;
+
+
+end.
